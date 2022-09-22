bcb84e41e3b2d5d5d10d47b34c4a8ea83228a309
Mateus Reis <you@example.com>
2022-09-22 16:00:40 -0300

    featuredProperties

---
+++ src/components/featuredProperties/FeaturedProperties.jsx
@@ -0,0 +1,50 @@
+import './featuredProperties.css'
+
+function FeaturedProperties() {
+  return (
+    <div className="fp">
+      <div className="fpItem">
+        <img className='fpImg' src="https://images.unsplash.com/photo-1548495010-83f5578e1f03?q=100&fm=jpg&w=500&fit=max" alt="" />
+        <span className="fpName">Aparthotel Stare Miastro</span>
+        <span className="fpCity">Madrid</span>
+        <span className="fpPrice">Starting from $120</span>
+        <div className="fpRating">
+          <button>8.9</button>
+          <span>Excellent</span>
+        </div>
+      </div>
+      <div className="fpItem">
+        <img className='fpImg' src="https://images.unsplash.com/photo-1548495010-83f5578e1f03?q=100&fm=jpg&w=500&fit=max" alt="" />
+        <span className="fpName">Aparthotel Stare Miastro</span>
+        <span className="fpCity">Madrid</span>
+        <span className="fpPrice">Starting from $120</span>
+        <div className="fpRating">
+          <button>8.9</button>
+          <span>Excellent</span>
+        </div>
+      </div>
+      <div className="fpItem">
+        <img className='fpImg' src="https://images.unsplash.com/photo-1548495010-83f5578e1f03?q=100&fm=jpg&w=500&fit=max" alt="" />
+        <span className="fpName">Aparthotel Stare Miastro</span>
+        <span className="fpCity">Madrid</span>
+        <span className="fpPrice">Starting from $120</span>
+        <div className="fpRating">
+          <button>8.9</button>
+          <span>Excellent</span>
+        </div>
+      </div>
+      <div className="fpItem">
+        <img className='fpImg' src="https://images.unsplash.com/photo-1548495010-83f5578e1f03?q=100&fm=jpg&w=500&fit=max" alt="" />
+        <span className="fpName">Aparthotel Stare Miastro</span>
+        <span className="fpCity">Madrid</span>
+        <span className="fpPrice">Starting from $120</span>
+        <div className="fpRating">
+          <button>8.9</button>
+          <span>Excellent</span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default FeaturedProperties
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,5 @@
 import Featured from '../../components/featured/Featured'
+import FeaturedProperties from '../../components/featuredProperties/FeaturedProperties'
 import Header from '../../components/header/Header'
 import Navbar from '../../components/navbar/Navbar'
 import PropertyList from '../../components/propertyList/PropertyList'
@@ -11,9 +12,10 @@ const Home = () => {
       <Header />
       <div className="homeContainer">
         <Featured />
-        <h1 className='homeTitle'>Browse by property type</h1>
+        <h1 className='homeTitle'>Pesquise por tipo de acomodação</h1>
         <PropertyList />
         <h1 className='homeTitle'>Homes guests love</h1>
+        <FeaturedProperties />
       </div>
     </div>
   )
