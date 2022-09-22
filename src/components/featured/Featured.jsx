37d256ee7e6e8ab28358064bf8deafbd2f6ec533
Mateus Reis <you@example.com>
2022-09-22 15:37:14 -0300

    propertyList e featured

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -2,7 +2,29 @@ import './featured.css'
 
 function Featured() {
   return (
-    <div>Featured</div>
+    <div className="featured">
+      <div className="featuredItem">
+        <img src="https://images.unsplash.com/photo-1575283757534-1a1999f6f341?q=100&fm=jpg&w=500&fit=max" alt="" />
+        <div className="featuredTitles">
+          <h1>Dublin</h1>
+          <h2>123 properties</h2>
+        </div>
+      </div>
+      <div className="featuredItem">
+        <img src="https://images.unsplash.com/photo-1557335200-a65f7f032602?q=100&fm=jpg&w=500&fit=max" alt="" />
+        <div className="featuredTitles">
+          <h1>Austin</h1>
+          <h2>123 properties</h2>
+        </div>
+      </div>
+      <div className="featuredItem">
+        <img src="https://images.unsplash.com/photo-1547406683-88dd1d03c425?q=100&fm=jpg&w=500&fit=max" alt="" />
+        <div className="featuredTitles">
+          <h1>Reno</h1>
+          <h2>123 properties</h2>
+        </div>
+      </div>
+    </div>
   )
 }
 
---
+++ src/components/propertyList/PropertyList.jsx
@@ -0,0 +1,45 @@
+import './propertyList.css'
+
+function PropertyList() {
+  return (
+    <div className="pList">
+      <div className="pListItem">
+        <img src="https://images.unsplash.com/photo-1455587734955-081b22074882?q=100&fm=jpg&w=500&fit=max" alt="" className="pListImg" />
+        <div className="pListTitles">
+          <h1>Hotels</h1>
+          <h2>233 hotels</h2>
+        </div>
+      </div>
+      <div className="pListItem">
+        <img src="https://images.unsplash.com/photo-1522708323590-d24dbb6b0267?q=100&fm=jpg&w=500&fit=max" alt="" className="pListImg" />
+        <div className="pListTitles">
+          <h1>Apartments</h1>
+          <h2>233 hotels</h2>
+        </div>
+      </div>
+      <div className="pListItem">
+        <img src="https://images.unsplash.com/photo-1617859047452-8510bcf207fd?q=100&fm=jpg&w=500&fit=max" alt="" className="pListImg" />
+        <div className="pListTitles">
+          <h1>Resorts</h1>
+          <h2>233 hotels</h2>
+        </div>
+      </div>
+      <div className="pListItem">
+        <img src="https://images.unsplash.com/photo-1582610116397-edb318620f90?q=100&fm=jpg&w=500&fit=max" alt="" className="pListImg" />
+        <div className="pListTitles">
+          <h1>Villas</h1>
+          <h2>233 hotels</h2>
+        </div>
+      </div>
+      <div className="pListItem">
+        <img src="https://images.unsplash.com/photo-1611821261180-8e6867e77288?q=100&fm=jpg&w=500&fit=max" alt="" className="pListImg" />
+        <div className="pListTitles">
+          <h1>Cabins</h1>
+          <h2>233 hotels</h2>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default PropertyList
